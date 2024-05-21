aa719ff25348148617001c39da743c9ae0db53fe
Joni <152844226+jonidm02@users.noreply.github.com>
2024-05-21 17:59:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -377,7 +377,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 user = await bot.get_chat_member(channel_chat_id, cmd.message.chat.id)
                 if user.status == "kicked":
                     await cmd.message.edit(
-                        text="Sorry Sir, You are Banned to use me. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/+YUFEObApJqIwMzBl).",
+                        text="Hello Friends [𝙅𝙊𝙄𝙉 𝙉𝙊𝙒](https://t.me/DM_MK_LinkZzz).",
                         disable_web_page_preview=True
                     )
                     return
